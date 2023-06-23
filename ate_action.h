562b33f8cc8563eea4757dec93a30c8908a1a36a
Chuck Jungmann <chuck@cpjj.net>
2023-06-23 11:21:12 -0500

    Add prototypes to functions to be used by new ate_delegate.c source file.

--- ate_action.h
+++ ate_action.h
@@ -11,18 +11,42 @@ typedef int (*ATE_ACTION)(const char *name_handle,
                           const char *name_array,
                           WORD_LIST *extra);
 
+
+int ate_action_declare(const char *name_handle, const char *name_value,
+                       const char *name_array, WORD_LIST *extra);
+
+int ate_action_get_row_count(const char *name_handle, const char *name_value,
+                             const char *name_array, WORD_LIST *extra);
+
+int ate_action_get_row(const char *name_handle, const char *name_value,
+                       const char *name_array, WORD_LIST *extra);
+
+int ate_action_put_row(const char *name_handle, const char *name_value,
+                       const char *name_array, WORD_LIST *extra);
+
+int ate_action_append_data(const char *name_handle, const char *name_value,
+                           const char *name_array, WORD_LIST *extra);
+
+
+
+
+
+
 typedef struct action_agent {
    const char *name;
    ATE_ACTION action;
    const char *description;
 } ATE_AGENT;
 
+/** @brief Found in ate_delegate.c
+ * @{
+ */
 int delegate_action(const char *name_action,
                     const char *name_handle,
                     const char *name_value,
                     const char *name_array,
                     WORD_LIST *extra);
-
+/** @} */
 
 
 #endif
