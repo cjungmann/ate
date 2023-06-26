5eabf594d34d659667636bb02d9e26ba2f88a2fb
Chuck Jungmann <chuck@cpjj.net>
2023-06-26 14:02:13 -0500

    managing actions' execution and self-documentation.

--- ate_delegate.c
+++ ate_delegate.c
@@ -11,11 +11,27 @@
 #include "ate_errors.h"
 
 ATE_AGENT agent_pool[] = {
-   { "declare",        ate_action_declare,       "Create a new ate object" }
-   , {"get_row_count", ate_action_get_row_count, "Get count of indexed rows" }
-   , {"get_row",       ate_action_get_row,       "Get row by index number" }
-   , {"put_row",       ate_action_put_row,       "Put row by index number" }
-   , {"append_data",   ate_action_append_data,   "Add data to end of table" }
+   { "show_actions", ate_action_show_actions, "Show available actions",
+     "ate show_actions"
+   }
+   , { "declare", ate_action_declare, "Create a new ate handle",
+     "ate declare name_handle name_array [row_size]"
+   }
+   , { "get_row_count", ate_action_get_row_count, "Get count of indexed rows",
+      "ate get_row_count name_handle [-v name_result ]"
+   }
+   , { "get_row", ate_action_get_row, "Get row by index number",
+      "ate get_row name_handle row_number [-a name_row_array]"
+   }
+   , { "put_row", ate_action_put_row, "Put row by index number",
+      "ate put_row name_handle row_number name_row_array"
+   }
+   , { "append_data",   ate_action_append_data,   "Add data to end of table",
+      "ate append_data name_handle [value ...]"
+   }
+   , { "walk_rows", ate_action_walk_rows, "invoke callback function with each row in table.",
+      "ate walk_rows name_handle name_callback [start_row] [row_count]"
+   }
    // , {"seek_row"       ate_seek_row"             "Seek row with callback" }
    // , {"seek_row_field" ate_seek_row_field"       "Seek row by field" }
 };
@@ -24,6 +40,22 @@ const int agent_count = sizeof(agent_pool) / sizeof(ATE_AGENT);
 
 /** Used to test for array limit in @ref delegate_action. */
 const ATE_AGENT *agent_limit = agent_pool + agent_count;
+
+/**
+ * @brief Prints the usage and description for each enumerated action.
+ */
+void delegate_show_action_usage(void)
+{
+   const ATE_AGENT *agent = (const ATE_AGENT*)agent_pool;
+   while (agent < agent_limit)
+   {
+      if (agent > (const ATE_AGENT*)agent_pool)
+         printf("\n");
+      printf("%s\n  %s\n", agent->description, agent->usage);
+
+      ++agent;
+   }
+}
 
 /**
  * @brief Assembles resources, finds appropriate agent, then executes
