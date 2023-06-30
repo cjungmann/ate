1953288b62335f9af7ecb1afca65eb1e1337fe30
Chuck Jungmann <chuck@cpjj.net>
2023-06-30 02:50:39 -0500

    three new utilities

--- ate_utilities.h
+++ ate_utilities.h
@@ -12,12 +12,18 @@
 
 #include "ate_handle.h"
 
+bool make_unique_name(char *buffer, int bufflen, const char *stem);
+
 bool get_string_from_list(const char **result, WORD_LIST *list, int index);
 bool get_int_from_list(int *result, WORD_LIST *list, int index);
 bool get_var_from_list(SHELL_VAR **result, WORD_LIST *list, int index);
 
+int set_var_from_int(SHELL_VAR *result, int value);
+
 void ate_dispose_variable_value(SHELL_VAR *var);
 SHELL_VAR *ate_get_prepared_variable(const char *name, int attributes);
+
+bool prepare_clean_array_var(SHELL_VAR **var, const char *name);
 
 int get_handle_from_name(AHEAD **head, const char *name_handle);
 
